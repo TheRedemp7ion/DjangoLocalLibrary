e3c49da272d863185681b2b934c45a4693054a7a
shashank <theredemp7ion@gmail.com>
2021-05-18 02:15:50 +0530

    HEROKU change

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -13,11 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import pytz
 import os # needed by code below
-import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -31,7 +30,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['obscure-plateau-04602.herokuapp.com']
 
 
 # Application definition
